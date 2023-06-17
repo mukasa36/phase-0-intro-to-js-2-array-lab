767a245adbbdcc6e411e514af6325686e18aee98
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-17 16:47:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+let cats =[ "Milo","Otis","Garfield"];
+
+function destructivelyAppendCat (Ralph){
+    cats.push(Ralph)
+    return cats;
+}
+
+function destructivelyPrependCat (Bob){
+    cats.unshift (Bob)
+    return cats;
+}
+
+function destructivelyRemoveLastCat (Garfield){
+    cats.pop(Garfield)
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(0)
+    return cats;
+}
+
+function appendCat (Broom){
+    cats = [...cats,"Broom"]
+    return cats;
+}
+
+function prependCat(Arnold,Broom){
+    cats.unshift (Arnold)
+    cats.pop(Broom)
+    return cats;
+}
+
+function removeLastCat(Arnold,Garfield){
+cats.shift (Arnold)
+cats.pop(Garfield)
+return cats;
+}
+
+function removeFirstCat (Milo,Garfield){
+    cats =[...cats,"Garfield"]
+    cats.shift(Milo)
+    return cats;
+}
+
+
+
+
+
+
